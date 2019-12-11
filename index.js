ee8bb4a34b493ef0b218fe88416491e750ce5aeb
Taylor Piccarreto <tpic12@gmail.com>
2019-12-11 12:13:04 -0500

    add quantity dogs

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ function getDogImage() {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let numDogs = document.getElementById('dogSearch').value;
+    console.log(numDogs);
+    if (numDogs === '') {
+        numDogs = 3;
+        for (let i=1; i<=numDogs; i++){
+            getDogImage();
+            }
+    }
+    else if (numDogs > 51) {
+        console.log('Please type a lower number');
+    }
+    else {
+      for (let i=1; i<=numDogs; i++){
+      getDogImage();
+      }
+    }
   });
 }
 
