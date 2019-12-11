f99e34d2bdd8992dca6eee6dd048e1269fd3a6c2
Taylor Piccarreto <tpic12@gmail.com>
2019-12-11 14:15:12 -0500

    breed branch

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
-}
+function getDogImage(str) {
+    fetch(`https://dog.ceo/api/breed/${str}/images/random`)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
+  
+  function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      let bree = document.getElementById('dogSearch').value;
+      let breed = bree.toLowerCase(document.getElementById('dogSearch').value);
 
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results').append(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
-}
+      getDogImage(breed);
+    });
+  }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    let numDogs = document.getElementById('dogSearch').value;
-    console.log(numDogs);
-    if (numDogs === '') {
-        numDogs = 3;
-        for (let i=1; i<=numDogs; i++){
-            getDogImage();
-            }
-    }
-    else if (numDogs > 51) {
-        console.log('Please type a lower number');
-    }
-    else {
-      for (let i=1; i<=numDogs; i++){
-      getDogImage();
+  function displayResults(responseJson) { 
+      console.log(responseJson); 
+      
+      $('.results').html( `<img src="${responseJson.message}" class="results-img">` );
+      $('.results').removeClass('hidden');
       }
-    }
-  });
-}
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
